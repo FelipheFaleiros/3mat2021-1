2f32b22001299bf482cfd92817c272354921e311
FelipheFaleiros <feliphefaleiros@hotmail.com>
2021-03-28 19:39:44 +0000

    (15/03) Término de selection sort e início de merge sort

--- procura-de-dados/07-refazer.mjs
+++ procura-de-dados/07-refazer.mjs
@@ -28,16 +28,29 @@ function selectionSort(vetor) {
         pass++
         let menor = encontrarMenor(i + 1)
         //console.log ('i:', vetor[i], "menor", vetor[menor])      //debugar
-        if (vetor[menor] < vetor[i]) [ vetor[menor], vetor[i] ] = [ vetor[i], vetor[menor] ]
-        trocas++
+        if (vetor[menor] < vetor[i]) {
+            [ vetor[menor], vetor[i] ] = [ vetor[i], vetor[menor] ]
+            trocas++
+        }
+        comps++
     }
-    comps++
 }
 
 
 
 
 let nums = [7,4,9,0,6,1,8,2,5,3]
+//let nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+console.log(nums)
 selectionSort(nums)
 console.log(nums)
+console.log({trocas,pass,comps})
+
+
+import { nomes } from "../includes/100-mil-nomes.mjs"
+console.log("Antes:", nomes)
+console.time("Ordenando Nomes...")
+selectionSort(nomes)
+console.timeEnd("Ordenando Nomes...")
 console.log({trocas,comps,pass})
+console.log("Depois:", nomes)
--- procura-de-dados/07-selection-sort.mjs
+++ procura-de-dados/07-selection-sort.mjs
@@ -1,41 +1,43 @@
-/* 
+/*
     SELECTION SORT
-
     Trata-se de uma otimização do bubble sort, diminuindo drasticamente
-    o número de trocas necessárias para fazer  a orde
-
+    o número de trocas necessárias para fazer a ordenação.
+    Isola um dos valores do vetor e procura pelo menor valor entre os
+    restantes, promovendo a troca caso o primeiro valor seja maior que
+    o segundo.
 */
-let  trocas, pass, comps
+let trocas, pass, comps
 
-function selectionSort(vetor){
+function selectionSort(vetor) {
 
     trocas = 0, pass = 0, comps = 0
-
+    
     function encontrarMenor(inicio) {
         let menor = inicio
-        for (let j = inicio + 1; j < vetor.length; j++){
-            if (vetor[j] < vetor[menor]) menor = j 
+        for(let j = inicio + 1; j < vetor.length; j++) {
+            if(vetor[j] < vetor[menor]) menor = j
             comps++
         }
         return menor
     }
-    //0 vetor será percorrido da primeira até a PENULTIMA posição 
-    for(let i=0; i <= vetor.length - 2; i++){
-        let menor = encontrarMenor(i+1)
+    
+    // O vetor será percorrido da primeira até a PENÚLTIMA posição
+    for(let i = 0; i <= vetor.length - 2; i++) {
         pass++
-        let menor = encontraMenor(i+1)
-        //console.log("i:", vetor[i], "menor:", vetor[menor])   debug
+        let menor = encontrarMenor(i + 1)
+        // console.log('i:', vetor[i], 'menor: ', vetor[menor])
         if(vetor[menor] < vetor[i]) {
-            [vetor [menor], vetor[i]] = [vetor[i], vetor[menor]]
+            [ vetor[menor], vetor[i] ] = [ vetor[i], vetor[menor] ]
             trocas++
         }
         comps++
     }
 }
 
-//let nums = [7,4,9,0,6,1,8,2,5,3]
-//Pior caso
-let nums = [9,8,7,6,5,4,3,2,1,0]
+let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
+// Pior caso
+//let nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+//let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 selectionSort(nums)
-//console.log(nums)
-//console.log({trocas, pass, comps})
+console.log(nums)
+console.log({trocas, pass, comps})
